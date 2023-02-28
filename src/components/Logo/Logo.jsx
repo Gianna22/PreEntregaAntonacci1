b63ff748b769fc97435a721d1fb11095c9ee8584
Gianna Antonacci <giannaantonacci1@gmail.com>
2023-02-28 09:17:08 -0300

    proyecto casi terminado

--- src/components/Logo/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Logo = () => {
-  return (
-   <img src='https://i.pinimg.com/564x/a0/d4/13/a0d4134f488ed03cd9200152bceff549.jpg' alt='logo'></img>
-  )
-}
-export default Logo
---
+++ src/utils/images.js
@@ -0,0 +1,21 @@
+import Bombacha from "..src/assets/bombachanena.jpg";
+import Pijama from "../src/assets/piyamabob.jpg";
+import Medias from "../src/assets/mediaslarfas.jpg";
+import Boxer from "../src/assets/boxers.jpg";
+import Slider01 from "../assets/slider01.webp";
+import Slider02 from "../assets/slider02.webp";
+import Slider03 from "../assets/slider03.webp";
+import Logo from "..src/assets/Logotipo Logo Marca personal Pastel Amarillo.png";
+import Logo01 from "../assets/logo01.gif";
+
+export const Images = {
+  Bombacha,
+  Pijama,
+  Medias,
+  Boxer,
+  Slider01,
+  Slider02,
+  Slider03,
+  Logo,
+  Logo01,
+};
